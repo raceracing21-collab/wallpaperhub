b64610821b4a8fcdfe67152dbf21ba99915f721d
raceracing21-collab <raceracing21@gmail.com>
2026-06-30 16:09:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log('RaceWalls v1.0');
+const grid = document.getElementById("wallpaperGrid");
+
+async function loadWallpapers() {
+
+    try {
+
+        const response = await fetch("data/wallpapers.json");
+
+        const wallpapers = await response.json();
+
+        showWallpapers(wallpapers);
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+
+}
+
+function showWallpapers(data){
+
+    grid.innerHTML="";
+
+    data.forEach(item=>{
+
+        grid.innerHTML += `
+
+        <div class="wallpaper-card">
+
+            <img src="${item.image}" alt="${item.title}">
+
+            <div class="overlay">
+
+                ${item.title}
+
+            </div>
+
+        </div>
+
+        `;
+
+    });
+
+}
+
+loadWallpapers();
